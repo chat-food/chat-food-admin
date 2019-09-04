f806ad0f964c9851746a83b6b3decb54fba307ee
Laboratorio <Laboratorio@LAB00XX.mshome.net>
2019-09-04 20:52:01 -0300

    Cria classes: Cardapio; ItemCardapio; ListagemCardapio

---
+++ src/chat/food/Cardapio.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chat.food;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+/**
+ *
+ * @author Laboratorio
+ */
+public class Cardapio {
+    
+    private int idCardapio;
+
+    public static final String PROP_IDCARDAPIO = "idCardapio";
+
+    public int getIdCardapio() {
+        return idCardapio;
+    }
+
+    public void setIdCardapio(int idCardapio) {
+        int oldIdCardapio = this.idCardapio;
+        this.idCardapio = idCardapio;
+        propertyChangeSupport.firePropertyChange(PROP_IDCARDAPIO, oldIdCardapio, idCardapio);
+    }
+
+    private transient final PropertyChangeSupport propertyChangeSupport = new PropertyChangeSupport(this);
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        propertyChangeSupport.addPropertyChangeListener(listener);
+    }
+
+    public void removePropertyChangeListener(PropertyChangeListener listener) {
+        propertyChangeSupport.removePropertyChangeListener(listener);
+    }
+}
